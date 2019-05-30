6c5deb915b9f0904ea032cc7a03b281fe6077854
Tommy Lindeberg <lindeberg.tommy@gmail.com>
2019-05-30 19:28:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 
-function accessElementInArray(){
-  
+function accessElementInArray(array, index){
+  return array[index];
 }
