c20e32498dc284804915266e6e311098eb5c3709
Tommy Lindeberg <lindeberg.tommy@gmail.com>
2019-05-30 19:18:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  ['$Element', ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
